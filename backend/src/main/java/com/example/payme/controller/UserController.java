eea79caf4899b023ed7338abdd583ba4430f5aad
Nihal <nihalyadav8524@gmail.com>
2026-08-03 13:14:31 +0000

    add user controller with get all , get user by id

---
+++ backend/src/main/java/com/example/payme/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.payme.controller;
+
+import com.example.payme.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.payme.model.User;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+}
